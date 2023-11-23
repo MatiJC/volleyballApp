75f2081f6557a437fff566ba5fc10260fcaf9199
MatiJC <mcuaderno@gmail.com>
2023-11-23 01:11:23 -0300

    Packages organized, user model and repo

---
+++ src/main/java/com/maticuad/volleyballApp/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.maticuad.volleyballApp.repositories;
+
+import com.maticuad.volleyballApp.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Optional;
+
+@RepositoryRestResource
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
