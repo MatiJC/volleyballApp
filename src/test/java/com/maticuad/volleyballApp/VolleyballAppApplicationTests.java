9192dc2aecad8b770d6c951e324ec998ed2e1913
MatiJC <mcuaderno@gmail.com>
2023-11-22 21:14:28 -0300

    First commit

---
+++ src/test/java/com/maticuad/volleyballApp/VolleyballAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maticuad.volleyballApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolleyballAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
